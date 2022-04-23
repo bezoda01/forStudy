9d4271af712b3212593aec1c3a1a3949549c14c5
bezoda01 <86252841+bezoda01@users.noreply.github.com>
2022-04-23 16:49:00 +0300

    hey

--- main.java
+++ main.java
@@ -1,6 +1,6 @@
 public class Demo {
     public static void main(String[] args) {
-        String welcome = "hi!";
+        String welcome = "Welcome!";
         for (int i = 0; i < 100; i++) {
             System.out.println(welcome);
         }
