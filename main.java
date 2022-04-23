f87fd577bee1c82a2f835b6804065e4c9b633496
bezoda01 <86252841+bezoda01@users.noreply.github.com>
2022-04-23 15:52:34 +0300

    some changes

--- main.java
+++ main.java
@@ -1,6 +1,8 @@
-public static void main(String[]args){
-    String welcome = "hi!";
-    for(int i = 0;i < 100;i++) {
-        System.out.println(welcome);
+public class Demo {
+    public static void main(String[] args) {
+        String welcome = "hi!";
+        for (int i = 0; i < 100; i++) {
+            System.out.println(welcome);
         }
+    }
 }
